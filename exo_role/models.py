f081410f5fc6ff6e11a919dcb956f6fc7173ada4
marfyl <marfyl.dev@gmail.com>
2019-10-22 13:03:13 +0200

    Update model properties

--- exo_role/models.py
+++ exo_role/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.conf import settings as settings_global
 
 from model_utils.models import TimeStampedModel
 
@@ -21,29 +20,3 @@ class ExORole(TimeStampedModel):
 
     def __str__(self):
         return self.name
-
-    @property
-    def is_job(self):
-        return self.code in settings.EXO_ROLES_PROJECT_WITH_JOBS
-
-    @property
-    def is_manager(self):
-        return self.code in [
-            settings_global.ROL_CH_HEAD_COACH,
-            settings_global.ROL_CH_CURATOR,
-            settings_global.ROL_CH_CO_CURATOR,
-        ]
-
-    @property
-    def group_by(self):
-        group_by = None
-
-        roles = [
-            settings_global.ROL_CH_TRAINER,
-            settings_global.ROL_CH_ALIGN_TRAINER,
-        ]
-
-        if self.code in roles:
-            group_by = settings_global.ROL_CH_EXO_TRAINER
-
-        return group_by
