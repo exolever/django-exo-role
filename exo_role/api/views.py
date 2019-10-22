a556f553de66ca1828386c21e547d0b2074d439b
marfyl <marfyl.dev@gmail.com>
2019-10-22 14:22:58 +0200

    Update views

--- exo_role/api/views.py
+++ exo_role/api/views.py
@@ -1,18 +1,12 @@
-from django_filters.rest_framework import DjangoFilterBackend
-
-from rest_framework import generics, filters
+from rest_framework import generics
 
 from .serializers import ExORoleSerializer, CategorySerializer
-from .filters import ExORoleFilter
 from ..models import ExORole, Category
 
 
 class ExORoleListView(generics.ListAPIView):
     queryset = ExORole.objects.all()
     serializer_class = ExORoleSerializer
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    filter_class = ExORoleFilter
-    ordering = ('name',)
 
 
 class CategoryListView(generics.ListAPIView):
